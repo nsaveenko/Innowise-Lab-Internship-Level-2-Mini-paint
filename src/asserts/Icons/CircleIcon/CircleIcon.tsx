8a72fe5ac3daf3e924f8200e4ad3b8799a47721a
nsaveenko <nn.saveenko@gmail.com>
2021-11-11 12:49:05 +0300

    feat: finish auth

--- src/asserts/Icons/CircleIcon/CircleIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { IIcon } from '../IIcon/IIcon';
-
-export default function CircleIcon({ width, height }: IIcon) {
-  return (
-    <svg stroke='currentColor' fill='currentColor' strokeWidth='0' viewBox='0 0 16 16' height={height} width={width} xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z' clipRule='evenodd' /></svg>
-  );
-}
