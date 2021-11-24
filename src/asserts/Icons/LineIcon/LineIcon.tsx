fcd8f1da7c1dadfc5e8695acb0b8f6f2fedfb968
nsaveenko <nn.saveenko@gmail.com>
2021-11-24 12:36:28 +0300

    refactor: rename and replace interfaces for icons

--- src/asserts/Icons/LineIcon/LineIcon.tsx
+++ src/asserts/Icons/LineIcon/LineIcon.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { IIcon } from '../IIcon/IIcon';
+interface IProps {
+  width: string,
+  height: string,
+}
 
-export default function LineIcon({ width, height }: IIcon) {
+export default function LineIcon({ width, height }: IProps) {
   return (
     <svg stroke='currentColor' fill='currentColor' strokeWidth='0' viewBox='0 0 1024 1024' height={height} width={width} xmlns='http://www.w3.org/2000/svg'>
       <path d='M904 476H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z' />
--- src/asserts/Icons/PenIcon/PenIcon.tsx
+++ src/asserts/Icons/PenIcon/PenIcon.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { IIcon } from '../IIcon/IIcon';
+interface IProps {
+  width: string,
+  height: string,
+}
 
-function PenIcon({ width, height }: IIcon) {
+function PenIcon({ width, height }: IProps) {
   return (
     <svg stroke='currentColor' fill='currentColor' strokeWidth='0' viewBox='0 0 16 16' height={height} width={width} xmlns='http://www.w3.org/2000/svg'>
       <path
